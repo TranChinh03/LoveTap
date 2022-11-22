980b101c9a83b3caa540e147f83813bc33bf97b7
TranChinh03 <21520166@gm.uit.edu.vn>
2022-11-23 00:59:03 +0700

    dev: Init Source

---
+++ LoveTap/LoveTap/ViewModel/GoodsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using LoveTap.Model;
+using LoveTap.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoveTap.ViewModel
+{
+    public class GoodsViewModel : BaseViewModel
+    {
+        //private ObservableCollection<SANPHAM> _ProductList;
+        //public ObservableCollection<SANPHAM> ProductList { get => _ProductList; set { _ProductList = value; OnPropertyChanged(); } }
+
+        //public GoodsViewModel()
+        //{
+        //    ProductList = new ObservableCollection<SANPHAM>(DataProvider.Ins.DB.SANPHAMs);
+        //}
+    }
+}
