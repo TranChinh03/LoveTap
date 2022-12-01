ebdd65310cb3826f03b267e79ada9fbe987868f9
TranChinh03 <95136995+TranChinh03@users.noreply.github.com>
2022-12-01 12:57:51 +0700

    Revert "orders + statistic"

--- LoveTap/LoveTap/ViewModel/MainViewModel.cs
+++ LoveTap/LoveTap/ViewModel/MainViewModel.cs
@@ -85,8 +85,7 @@ namespace LoveTap.ViewModel
         public MainViewModel()
         {
 
-            LoadedHomeCommand = new RelayCommand<Window>((p) => { return true; }, (p) =>
-            {
+            LoadedHomeCommand = new RelayCommand<Window>((p) => { return true; },(p)=> {
                 IsLoaded = true;
                 if (p == null)
                     return;
@@ -96,7 +95,7 @@ namespace LoveTap.ViewModel
 
                 if (loginWindow.DataContext == null)
                     return;
-                var loginVM = loginWindow.DataContext as LoginViewModel;
+                var loginVM=loginWindow.DataContext as LoginViewModel;
                 if (loginVM.IsLogin)
                 {
                     p.Show();
@@ -108,8 +107,6 @@ namespace LoveTap.ViewModel
             });
 
             //LoadedHomeCommand = new RelayCommand<object>((p) => { return true; }, (p) => { HomeWindow homeWindow = new HomeWindow(); homeWindow.ShowDialog(); });
-
-
         }
     }
 }
