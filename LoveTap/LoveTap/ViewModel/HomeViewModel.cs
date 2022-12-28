72b6ab7190c2ae7ef24c4bc8e329b10549b4d694
21522611@gm.uit.edu.vn <21522611@gm.uit.edu.vn>
2022-12-28 11:20:45 +0700

    Statistic screenHome and binding SelectedDate from Calendar

--- LoveTap/LoveTap/ViewModel/HomeViewModel.cs
+++ LoveTap/LoveTap/ViewModel/HomeViewModel.cs
@@ -9,10 +9,11 @@ using System.Windows.Controls;
 using System.Windows;
 using System.Windows.Input;
 using LoveTap.Stores;
+using LoveTap.UserControlCustom;
 
 namespace LoveTap.ViewModel
 {
-    public class HomeViewModel: BaseViewModel
+    public class HomeViewModel : BaseViewModel
     {
 
         private ObservableCollection<SANPHAM> _ProductList;
@@ -23,6 +24,13 @@ namespace LoveTap.ViewModel
 
         private ObservableCollection<TONKHO> _StockList;
         public ObservableCollection<TONKHO> StockList { get => _StockList; set { _StockList = value; OnPropertyChanged(); } }
+
+        private ObservableCollection<HOADON> _OrderList;
+        public ObservableCollection<HOADON> OrderList { get => _OrderList; set { _OrderList = value; OnPropertyChanged(); } }
+
+        private ObservableCollection<KHACHHANG> _CustomerList;
+        public ObservableCollection<KHACHHANG> CustomerList { get => _CustomerList; set { _CustomerList = value; OnPropertyChanged(); } }
+
 
         private string _ProductID1;
         public string ProductID1 { get => _ProductID1; set { _ProductID1 = value; OnPropertyChanged(); } }
@@ -38,14 +46,22 @@ namespace LoveTap.ViewModel
 
         private string _Stock;
         public string Stock { get => _Stock; set { _Stock = value; OnPropertyChanged(); } }
-        public ICommand GetIdButton { get; set; }
+        public ICommand ChangeDate { get; set; }
 
         public ICommand SwitchTab { get; set; }
         public ICommand Detail { get; set; }
 
         public ICommand LoadHome { get; set; }
         string Name;
-
+        public Nullable<double> _Doanhthu { get; set; }
+        public Nullable<double> Doanhthu { get => _Doanhthu; set { _Doanhthu = value; OnPropertyChanged(); } }
+        public DateTime _selectedDate { get; set; }
+        public DateTime selectedDate { get => _selectedDate; set {_selectedDate = value; OnPropertyChanged(); } }
+        public int _slOrders { get; set; }
+        public int slOrders { get => _slOrders; set { _slOrders = value; OnPropertyChanged(); } }
+        public int _slKhachHang { get; set; }
+        public int slKhachHang { get => _slKhachHang; set { _slKhachHang = value; OnPropertyChanged(); } }
+        public int SoLuongTon { get; set; }
         public struct BestSelling
         {
             public string Ten { get; set; }
@@ -54,52 +70,54 @@ namespace LoveTap.ViewModel
         }
 
         private List<BestSelling> _BestSellingList = new List<BestSelling>();
-        public List<BestSelling> BestSellingList { get => _BestSellingList ; set { _BestSellingList = value; } }
+        public List<BestSelling> BestSellingList { get => _BestSellingList; set { _BestSellingList = value; } }
         public HomeViewModel(NavigationStore navigationStore)
         {
-            
-            LoadHome = new RelayCommand<Page>((p) => true, (p) =>
+            //Load Home
+            LoadHome = new RelayCommand<HomeViewUC>((p) => true, (p) =>
             {
                 ProductList = new ObservableCollection<SANPHAM>(DataProvider.Ins.DB.SANPHAMs);
+                OrderList = new ObservableCollection<HOADON>(DataProvider.Ins.DB.HOADONs);
+                CustomerList = new ObservableCollection<KHACHHANG>(DataProvider.Ins.DB.KHACHHANGs);
                 OrdersDetailList = new ObservableCollection<CTHD>(DataProvider.Ins.DB.CTHDs);
                 StockList = new ObservableCollection<TONKHO>(DataProvider.Ins.DB.TONKHOes);
 
-                int[] sum= new int [DataProvider.Ins.DB.SANPHAMs.Count()];
-                string[] name = new string [DataProvider.Ins.DB.SANPHAMs.Count()];
+                int[] sum = new int[DataProvider.Ins.DB.SANPHAMs.Count()];
+                string[] name = new string[DataProvider.Ins.DB.SANPHAMs.Count()];
                 int[] stock = new int[DataProvider.Ins.DB.SANPHAMs.Count()];
 
 
                 for (int i = 0; i < DataProvider.Ins.DB.SANPHAMs.Count(); i++)
                 {
                     int tong = 0;
-                    for(int j = 0; j < DataProvider.Ins.DB.CTHDs.Count(); j++)
+                    for (int j = 0; j < DataProvider.Ins.DB.CTHDs.Count(); j++)
                     {
                         if (ProductList[i].MASP == OrdersDetailList[j].MASP)
                             tong += (int)OrdersDetailList[j].SOLUONG;
                     }
                     sum[i] = tong;
                     name[i] = ProductList[i].TEN.ToString();
-                    for(int k = 0; k < DataProvider.Ins.DB.TONKHOes.Count(); k++)
+                    for (int k = 0; k < DataProvider.Ins.DB.TONKHOes.Count(); k++)
                     {
                         if (ProductList[i].MASP.ToString()==StockList[k].MASP.ToString())
-                            stock [i] = int.Parse(StockList[k].SOLUONG.ToString());
+                            stock[i] = int.Parse(StockList[k].SOLUONG.ToString());
                     }
                 }
 
-                for(int i=0;i<sum.Length -1 ;i++)
+                for (int i = 0; i<sum.Length -1; i++)
                 {
-                    for(int j= i + 1;j<sum.Length;j++)
+                    for (int j = i + 1; j<sum.Length; j++)
                     {
                         if (sum[j] > sum[i])
                         {
-                            Swap1(ref sum[j],ref sum[i]);
-                            Swap2(ref name[j],ref name[i]);
+                            Swap1(ref sum[j], ref sum[i]);
+                            Swap2(ref name[j], ref name[i]);
                             Swap1(ref stock[j], ref stock[i]);
                         }
                     }
                 }
 
-                for (int i= 0;i<sum.Length ;i++)
+                for (int i = 0; i<sum.Length; i++)
                 {
                     BestSelling best = new BestSelling();
                     best.Ten = name[i];
@@ -107,9 +125,15 @@ namespace LoveTap.ViewModel
                     best.SoLuongTon = stock[i];
                     BestSellingList.Add(best);
                 }
-
-
+                Statistic();
             });
+
+            //SelectedDay
+            void _SelectedDate(Calendar p)
+            {
+                selectedDate= p.SelectedDate.Value;
+                Statistic();
+            }
 
             void Swap1(ref int a, ref int b)
             {
@@ -125,32 +149,33 @@ namespace LoveTap.ViewModel
                 b = temp;
             }
 
-            GetIdButton = new RelayCommand<Button>((p) => true, (p) => Name = p.Uid);
-            SwitchTab = new RelayCommand<HomeScreen>((p) => true, (p) => switchtab(p));
-            Detail = new RelayCommand<HomeScreen>((p) => { return p.BestSellingList.SelectedItem == null ? false : true; }, (p) => _DetailCs(p));
+            ChangeDate = new RelayCommand<Calendar>((p) => { return p.SelectedDates == null ? false : true; }, (p) => _SelectedDate(p));
+            //Detail = new RelayCommand<HomeViewUC>((p) => { return p.cale.SelectedItem == null ? false : true; }, (p) => _DetailCs(p));
+            selectedDate= DateTime.Now.Date;
 
+        }
+
+        void Statistic()
+        {
+            slKhachHang = 0; slOrders= 0; Doanhthu = 0;
+            for (int i = 0; i < DataProvider.Ins.DB.HOADONs.Count(); i++)
+                if (OrderList[i].NGMUA==selectedDate)
+                {
+                    slOrders++;
+                    Doanhthu += OrderList[i].TONGTIEN;
+                };
+            for (int i = 0; i < DataProvider.Ins.DB.KHACHHANGs.Count(); i++)
+                if (CustomerList[i].NGDK==selectedDate)
+                    slKhachHang++;
         }
 
         
 
-        void switchtab(HomeScreen p)
-        {
-            int index = int.Parse(Name);
-            switch (index)
-            {
-                case 1:
-                    {
-                        //p.AddCustomer.Visibility=Visibility.Visible;
-                        break;
-                    }
-            }
-        }
-
         void _DetailCs(HomeScreen p)
         {
-           
+
             p.DetailBestSl.Visibility= Visibility.Visible;
-            
+
         }
 
     }
